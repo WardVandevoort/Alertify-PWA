7278772d052996ed039dbcacca60a9ef0d1c1fcb
GlennVanLaere <55496118+GlennVanLaere@users.noreply.github.com>
2021-04-15 16:51:27 +0200

    profile page initial design + reusable topnav build

---
+++ js/topnav-info.js
@@ -0,0 +1,9 @@
+document.write(`
+
+<nav class="topnav-info">
+     <a href="javascript:history.back()" class="backarrow"><img src="media/img/topnav/arrow-back.svg" alt="arrow-back"></a>
+     <div class="textbox-info"> <h2>Info</h2></div>
+     <a href="#" class="logo-favicon-topnav"><img src="media/img/topnav/logo_favicon.svg" alt="logo favicon"></a>
+</nav>
+
+`);

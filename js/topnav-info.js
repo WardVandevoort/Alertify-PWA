734cc0f60c6fec8f508c3c271cbad1f9206ab4a3
GlennVanLaere <55496118+GlennVanLaere@users.noreply.github.com>
2021-04-16 14:15:18 +0200

    medical info page created + bug & redundancy fixes to top and bottom nav

--- js/topnav-info.js
+++ js/topnav-info.js
@@ -1,9 +1,23 @@
-document.write(`
+let currentUrl = window.location.pathname;
 
-<nav class="topnav-info">
-     <a href="javascript:history.back()" class="backarrow"><img src="media/img/topnav/arrow-back.svg" alt="arrow-back"></a>
-     <div class="textbox-info"> <h2>Info</h2></div>
-     <a href="#" class="logo-favicon-topnav"><img src="media/img/topnav/logo_favicon.svg" alt="logo favicon"></a>
-</nav>
+if(currentUrl = '/profile'){
+     
+               document.write(`
 
-`);
+               <nav class="topnav-info">
+               <a href="javascript:history.back()" class="backarrow"><img src="./../media/img/topnav/arrow-back.svg" alt="arrow-back"></a>
+               <div class="textbox-info"> <h2>Info</h2></div>
+               <a href="#" class="logo-favicon-topnav"><img src="./../media/img/topnav/logo_favicon.svg" alt="logo favicon"></a>
+               </nav>
+               `);
+}
+else{
+     document.write(`
+
+          <nav class="topnav-info">
+          <a href="javascript:history.back()" class="backarrow"><img src="media/img/topnav/arrow-back.svg" alt="arrow-back"></a>
+          <div class="textbox-info"> <h2>Info</h2></div>
+          <a href="#" class="logo-favicon-topnav"><img src="media/img/topnav/logo_favicon.svg" alt="logo favicon"></a>
+          </nav>
+          `);
+     }

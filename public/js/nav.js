ac33d113f5a6decbd94bacf002e7f7eaaba3bf2f
GlennVanLaere <55496118+GlennVanLaere@users.noreply.github.com>
2021-04-20 14:16:36 +0200

    re added ice and medical

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,10 +1,27 @@
-document.write(`
 
-<nav class="bottom-nav">
-     <a href="index.html" class="nav-link home-link"><img src="media/img/home_icon.svg" alt="home icon"></a>
-     <a href="profile.html" class="nav-link profile-link"><img src="media/img/profile_icon.svg" alt="profile icon"></a>
-     <a href="#" class="nav-link course-link"><img src="media/img/course_icon.svg" alt="course icon"></a>
-     <a href="#" class="nav-link settings-link"><img src="media/img/settings_icon.svg" alt="settings icon"></a>
-</nav>
 
-`);
+if(currentUrl = '/profile'){
+     
+     document.write(`
+
+     <nav class="bottom-nav">
+          <a href="./../../index.html" class="nav-link home-link"><img src="./../media/img/home_icon.svg" alt="home icon"></a>
+          <a href="./../../profile.html" class="nav-link profile-link"><img src="./../media/img/profile_icon.svg" alt="profile icon"></a>
+          <a href="#" class="nav-link course-link"><img src="./../media/img/course_icon.svg" alt="course icon"></a>
+          <a href="#" class="nav-link settings-link"><img src="./../media/img/settings_icon.svg" alt="settings icon"></a>
+     </nav>
+     
+     `);
+}
+else{
+     document.write(`
+
+     <nav class="bottom-nav">
+          <a href="index.html" class="nav-link home-link"><img src="media/img/home_icon.svg" alt="home icon"></a>
+          <a href="profile.html" class="nav-link profile-link"><img src="media/img/profile_icon.svg" alt="profile icon"></a>
+          <a href="#" class="nav-link course-link"><img src="media/img/course_icon.svg" alt="course icon"></a>
+          <a href="#" class="nav-link settings-link"><img src="media/img/settings_icon.svg" alt="settings icon"></a>
+     </nav>
+     
+     `);
+}

e6c38a0672a64079efd606d723f656d238f6887b
GlennVanLaere <55496118+GlennVanLaere@users.noreply.github.com>
2021-04-28 16:48:00 +0200

    bottom and top navigation routing bugs fixed

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,27 +1,36 @@
+document.write(`
 
+<nav class="bottom-nav">
+     <a href="index.html" class="nav-link home-link"><img src="media/img/home_icon.svg" alt="home icon"></a>
+     <a href="profile.html" class="nav-link profile-link"><img src="media/img/profile_icon.svg" alt="profile icon"></a>
+     <a href="#" class="nav-link course-link"><img src="media/img/course_icon.svg" alt="course icon"></a>
+     <a href="#" class="nav-link settings-link"><img src="media/img/settings_icon.svg" alt="settings icon"></a>
+</nav>
 
-if(currentUrl = '/profile'){
-     
+`);
+
+if(window.location.href.indexOf("change") >1){
+     document.write(`
+
+<nav class="bottom-nav">
+     <a href="./../../index.html" class="nav-link home-link"><img src="./../../media/img/home_icon.svg" alt="home icon"></a>
+     <a href="./../../profile.html" class="nav-link profile-link"><img src="./../../media/img/profile_icon.svg" alt="profile icon"></a>
+     <a href="#" class="nav-link course-link"><img src="./../../media/img/course_icon.svg" alt="course icon"></a>
+     <a href="#" class="nav-link settings-link"><img src="./../../media/img/settings_icon.svg" alt="settings icon"></a>
+</nav>
+
+`);
+}
+else if(window.location.href.indexOf("/profile") >1){
      document.write(`
 
      <nav class="bottom-nav">
-          <a href="./../../index.html" class="nav-link home-link"><img src="./../media/img/home_icon.svg" alt="home icon"></a>
-          <a href="./../../profile.html" class="nav-link profile-link"><img src="./../media/img/profile_icon.svg" alt="profile icon"></a>
+          <a href="./../index.html" class="nav-link home-link"><img src="./../media/img/home_icon.svg" alt="home icon"></a>
+          <a href="./../profile.html" class="nav-link profile-link"><img src="./../media/img/profile_icon.svg" alt="profile icon"></a>
           <a href="#" class="nav-link course-link"><img src="./../media/img/course_icon.svg" alt="course icon"></a>
           <a href="#" class="nav-link settings-link"><img src="./../media/img/settings_icon.svg" alt="settings icon"></a>
      </nav>
      
      `);
 }
-else{
-     document.write(`
 
-     <nav class="bottom-nav">
-          <a href="index.html" class="nav-link home-link"><img src="media/img/home_icon.svg" alt="home icon"></a>
-          <a href="profile.html" class="nav-link profile-link"><img src="media/img/profile_icon.svg" alt="profile icon"></a>
-          <a href="#" class="nav-link course-link"><img src="media/img/course_icon.svg" alt="course icon"></a>
-          <a href="#" class="nav-link settings-link"><img src="media/img/settings_icon.svg" alt="settings icon"></a>
-     </nav>
-     
-     `);
-}
--- public/js/topnav-info.js
+++ public/js/topnav-info.js
@@ -1,23 +1,31 @@
-let currentUrl = window.location.pathname;
 
-if(currentUrl = '/profile'){
-     
-               document.write(`
-
-               <nav class="topnav-info">
-               <a href="javascript:history.back()" class="backarrow"><img src="./../media/img/topnav/arrow-back.svg" alt="arrow-back"></a>
-               <div class="textbox-info"> <h2>Info</h2></div>
-               <a href="#" class="logo-favicon-topnav"><img src="./../media/img/topnav/logo_favicon.svg" alt="logo favicon"></a>
-               </nav>
-               `);
+if(window.location.href.indexOf("change") > -1){
+     console.log("change");
+     document.write(`
+     <nav class="topnav-info">
+     <a href="javascript:history.back()" class="backarrow"><img src="./../../media/img/topnav/arrow-back.svg" alt="arrow-back"></a>
+     <div class="textbox-info"> <h2>Info</h2></div>
+     <a href="#" class="logo-favicon-topnav"><img src="./../../media/img/topnav/logo_favicon.svg" alt="logo favicon"></a>
+     </nav>
+     `);
+}
+else if(window.location.href.indexOf("/profile") > -1){
+     console.log("profile");
+     document.write(`
+     <nav class="topnav-info">
+     <a href="javascript:history.back()" class="backarrow"><img src="./../media/img/topnav/arrow-back.svg" alt="arrow-back"></a>
+     <div class="textbox-info"> <h2>Info</h2></div>
+     <a href="#" class="logo-favicon-topnav"><img src="./../media/img/topnav/logo_favicon.svg" alt="logo favicon"></a>
+     </nav>
+     `);
 }
 else{
      document.write(`
-
-          <nav class="topnav-info">
-          <a href="javascript:history.back()" class="backarrow"><img src="media/img/topnav/arrow-back.svg" alt="arrow-back"></a>
-          <div class="textbox-info"> <h2>Info</h2></div>
-          <a href="#" class="logo-favicon-topnav"><img src="media/img/topnav/logo_favicon.svg" alt="logo favicon"></a>
-          </nav>
-          `);
-     }
+     <nav class="topnav-info">
+     <a href="javascript:history.back()" class="backarrow"><img src="media/img/topnav/arrow-back.svg" alt="arrow-back"></a>
+     <div class="textbox-info"> <h2>Info</h2></div>
+     <a href="#" class="logo-favicon-topnav"><img src="media/img/topnav/logo_favicon.svg" alt="logo favicon"></a>
+     </nav>
+     `);
+}
+     

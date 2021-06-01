91d8f5f7c8f5388f803270b195c12785e66dad17
GlennVanLaere <55496118+GlennVanLaere@users.noreply.github.com>
2021-06-01 16:53:38 +0200

    courses base page added

--- public/js/nav.js
+++ public/js/nav.js
@@ -3,7 +3,7 @@ document.write(`
 <nav class="bottom-nav">
      <a href="index.html" class="nav-link home-link"><img src="media/img/home_icon.svg" alt="home icon"></a>
      <a href="profile.html" class="nav-link profile-link"><img src="media/img/profile_icon.svg" alt="profile icon"></a>
-     <a href="#" class="nav-link course-link"><img src="media/img/course_icon.svg" alt="course icon"></a>
+     <a href="courses.html" class="nav-link course-link"><img src="media/img/course_icon.svg" alt="course icon"></a>
      <a href="settings/settings.html" class="nav-link settings-link"><img src="media/img/settings_icon.svg" alt="settings icon"></a>
 </nav>
 
@@ -15,7 +15,7 @@ if(window.location.href.indexOf("change") > -1){
 <nav class="bottom-nav">
      <a href="./../../index.html" class="nav-link home-link"><img src="./../../media/img/home_icon.svg" alt="home icon"></a>
      <a href="./../../profile.html" class="nav-link profile-link"><img src="./../../media/img/profile_icon.svg" alt="profile icon"></a>
-     <a href="#" class="nav-link course-link"><img src="./../../media/img/course_icon.svg" alt="course icon"></a>
+     <a href="./../../courses.html" class="nav-link course-link"><img src="./../../media/img/course_icon.svg" alt="course icon"></a>
      <a href="./../../settings/settings.html" class="nav-link settings-link"><img src="./../../media/img/settings_icon.svg" alt="settings icon"></a>
 </nav>
 
@@ -28,8 +28,8 @@ else if(window.location.href.indexOf("add") > -1){
      <nav class="bottom-nav">
           <a href="./../../index.html" class="nav-link home-link"><img src="./../../media/img/home_icon.svg" alt="home icon"></a>
           <a href="./../../profile.html" class="nav-link profile-link"><img src="./../../media/img/profile_icon.svg" alt="profile icon"></a>
-          <a href="#" class="nav-link course-link"><img src="./../../media/img/course_icon.svg" alt="course icon"></a>
-          <a href="#" class="nav-link settings-link"><img src="./../../media/img/settings_icon.svg" alt="settings icon"></a>
+          <a href="./../../courses.html" class="nav-link course-link"><img src="./../../media/img/course_icon.svg" alt="course icon"></a>
+          <a href="./../../" class="nav-link settings-link"><img src="./../../media/img/settings_icon.svg" alt="settings icon"></a>
      </nav>
      
      `);
@@ -41,7 +41,7 @@ else if(window.location.href.indexOf("/profile") >1){
      <nav class="bottom-nav">
           <a href="./../index.html" class="nav-link home-link"><img src="./../media/img/home_icon.svg" alt="home icon"></a>
           <a href="./../profile.html" class="nav-link profile-link"><img src="./../media/img/profile_icon.svg" alt="profile icon"></a>
-          <a href="#" class="nav-link course-link"><img src="./../media/img/course_icon.svg" alt="course icon"></a>
+          <a href="./../courses.html" class="nav-link course-link"><img src="./../media/img/course_icon.svg" alt="course icon"></a>
           <a href="./../settings/settings.html" class="nav-link settings-link"><img src="./../media/img/settings_icon.svg" alt="settings icon"></a>
      </nav>
      

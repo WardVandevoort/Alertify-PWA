8c5e5164ba72110bd2cccd60cb541c8ee75a3a0f
Ward Vandevoort <vandevoort.ward@gmail.com>
2021-06-12 15:19:24 +0200

    Debugging settings

--- public/js/nav.js
+++ public/js/nav.js
@@ -51,10 +51,10 @@ else if(window.location.href.indexOf("settings") >-1){
      document.write(`
 
      <nav class="bottom-nav">
-          <a href="./../index.html" class="nav-link home-link"><img src="./../media/img/home_icon.svg" alt="home icon"></a>
-          <a href="./../profile.html" class="nav-link profile-link"><img src="./../media/img/profile_icon.svg" alt="profile icon"></a>
-          <a href="./../courses.html" class="nav-link course-link"><img src="./../media/img/course_icon.svg" alt="course icon"></a>
-          <a href="./../Settings/settings.html" class="nav-link settings-link"><img src="./../media/img/settings_icon.svg" alt="settings icon"></a>
+          <a href="./../index.html" class="nav-link home-link"><img src="../media/img/home_icon.svg" alt="home icon"></a>
+          <a href="./../profile.html" class="nav-link profile-link"><img src="../media/img/profile_icon.svg" alt="profile icon"></a>
+          <a href="./../courses.html" class="nav-link course-link"><img src="../media/img/course_icon.svg" alt="course icon"></a>
+          <a href="./../Settings/settings.html" class="nav-link settings-link"><img src="../media/img/settings_icon.svg" alt="settings icon"></a>
      </nav>
      
      `);
